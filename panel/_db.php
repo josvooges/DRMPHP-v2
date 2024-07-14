8631c08c43488d7a8ed14f096fdd977e0c3698c0
Jeanmar <81588882+josvooges@users.noreply.github.com>
2024-07-15 01:08:16 +0200

    Add files via upload

---
+++ panel/_db.php
@@ -0,0 +1,7 @@
+<?php
+$DBHost = "127.0.0.1";
+$DBUser = "admin";
+$DBPass = 'passwd';
+$DBName = "drm";
+
+?>
---
+++ panel/_sidebar.php
@@ -0,0 +1,54 @@
+<div class="vertical-menu">
+  <div data-simplebar class="h-100">
+    <div id="sidebar-menu">
+      <ul class="metismenu list-unstyled" id="side-menu">
+        <li class="menu-title" key="t-menu">Menu</li>
+        <li>
+          <a href="dashboard.php" class="waves-effect">
+            <i class="bx bx-desktop"></i>
+            <span>Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a href="index.php" class="waves-effect">
+            <i class="bx bx-tv"></i>
+            <span>Channels</span>
+          </a>
+        </li>
+        <li>
+          <a href="cats.php" class="waves-effect">
+            <i class="bx bx-news"></i>
+            <span>Categories</span>
+          </a>
+        </li>
+        <li>
+  <a href="lines.php" class="waves-effect">
+    <i class="bx bx-user"></i>
+    <span>Lines</span>
+  </a>
+  <ul class="sub-menu">
+    <li>
+      <a href="add_line.php" class="waves-effect">Add Line</a>
+    </li>
+    <li>
+      <a href="lines.php" class="waves-effect">Show Lines</a>
+    </li>
+  </ul>
+</li>
+        <li>
+          <a href="settings.php" class="waves-effect">
+            <i class="bx bx-cog"></i>
+            <span>Settings</span>
+          </a>
+        </li> 
+
+        <li>
+          <a href="login.php" class="waves-effect">
+            <i class="bx bx-log-out"></i>
+            <span>Logout</span>
+          </a>
+        </li> 
+      </ul>
+    </div>
+  </div>
+</div>
